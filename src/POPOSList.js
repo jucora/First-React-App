6aa95ee21702f01d6a8567dcdc786903aa599f67
jucora <artjulius@hotmail.com>
2020-07-01 01:51:05 -0500

    content component built

---
+++ src/POPOSList.js
@@ -0,0 +1,39 @@
+// src/POPOSList.js
+
+import React from "react";
+import POPOSSpace from "./POPOSSpace";
+
+function POPOSList() {
+  return (
+    <div className="POPOSList">
+      <POPOSSpace
+        name="50 California Street"
+        address="50 California St."
+        image="50-california-st.jpg"
+      />
+      <POPOSSpace name="Pine" address="Pine Street St." image="100-pine.jpg" />
+      <POPOSSpace
+        name="Market Street Plaza"
+        address="Market Street."
+        image="525-market-street-plaza.jpg"
+      />
+      <POPOSSpace
+        name="Embarcadero"
+        address="Embarcadero St."
+        image="embarcadero-center.jpg"
+      />
+      <POPOSSpace
+        name="Transamerica"
+        address="Transamerica St."
+        image="transamerica-redwood-park.jpg"
+      />
+      <POPOSSpace
+        name="Garden Terrace"
+        address="Garden Terrace St."
+        image="garden-terrace-at-150-california.jpg"
+      />
+    </div>
+  );
+}
+
+export default POPOSList;
---
+++ src/POPOSSpace.js
@@ -0,0 +1,21 @@
+// src/Project.js
+
+import React from "react";
+
+function POPOSSpace(props) {
+  const { name, image, address } = props;
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img
+        src={`${process.env.PUBLIC_URL}images/${image}`}
+        width="300"
+        height="300"
+        alt="Hello"
+      />
+      <div>{address}</div>
+    </div>
+  );
+}
+
+export default POPOSSpace;
