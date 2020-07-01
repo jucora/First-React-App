1a8e7e91be544bc6e6dfe21513825b5d221d985e
jucora <artjulius@hotmail.com>
2020-07-01 02:18:19 -0500

    dynamic content and footer

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footerContainer">
+      <footer>
+        <p>Julian Belmonte copyright 2019</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,11 +1,12 @@
 // src/Project.js
 
 import React from "react";
+import "./POPOSSpace.css";
 
 function POPOSSpace(props) {
   const { name, image, address } = props;
   return (
-    <div>
+    <div className="POPOSSpace">
       <h1>{name}</h1>
       <img
         src={`${process.env.PUBLIC_URL}images/${image}`}
